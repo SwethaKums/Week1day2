b646d7f29f5009efbeac25d359a71652bbb66bed
SwethaKums <104850290+SwethaKums@users.noreply.github.com>
2022-05-05 12:19:26 +0530

    Add files via upload

---
+++ week1/day2/MyCar.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class MyCar {
+
+public static void main(String[] args) {
+	Car obj=new Car();
+	obj.applyBreak();
+	obj.applyAccelerate();		
+    obj.applyGear();
+    obj.swtichonAc();
+    
+
+	}
+
+}
