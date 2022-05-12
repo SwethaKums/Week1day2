dbe26117a9209d37d6346f0a7e6f844646ea8cdb
SwethaKums <104850290+SwethaKums@users.noreply.github.com>
2022-05-12 11:31:26 +0530

    Add files via upload

---
+++ week2 day2/FaceBook2.java
@@ -0,0 +1,53 @@
+package week2.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FaceBook2 {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://www.facebook.com/");	
+		driver.manage().window().maximize();
+		
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.linkText("Create New Account")).click();
+		driver.findElement(By.name("firstname")).sendKeys("Swetha");
+		driver.findElement(By.name("lastname")).sendKeys("K");
+		driver.findElement(By.name("reg_email__")).sendKeys("9092992231");
+		driver.findElement(By.id("password_step_input")).sendKeys("Logeswari@19");
+		
+		//for selecting the date
+		WebElement daydropdown=driver.findElement(By.id("day"));
+		Select obj1=new Select(daydropdown);
+        obj1.selectByValue("31");
+                                          
+        //for selecting the month
+        WebElement monthdropdown=driver.findElement(By.id("month"));
+		Select obj2=new Select(monthdropdown);
+		obj2.selectByValue("5");
+		
+		//for selecting the year
+		WebElement yeardropdown=driver.findElement(By.id("year"));
+		Select obj3=new Select(yeardropdown);
+		obj3.selectByValue("2000");
+		
+	    // for selecting the Gender
+	    //for selecting female
+		
+		driver.findElement(By.name("sex")).click();
+		driver.findElement(By.name("websubmit")).click();
+		
+		
+		// for closing the application
+		//driver.close();
+	
+	}
+}
